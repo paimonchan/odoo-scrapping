7997a3070ca259d3411e0ec1a79cd9a6e60cdc5b
Paimon <n.h.shounen@gmail.com>
2023-07-02 14:24:34 +0700

    [NA] return as csv

--- controllers/scrapping_ucis.py
+++ controllers/scrapping_ucis.py
@@ -1,4 +1,5 @@
 from odoo.http import Controller, Response, route, request
+from odoo.http import request
 
 class ScrappingUcis(Controller):
     def _get_error(self, message):
@@ -7,10 +8,23 @@ class ScrappingUcis(Controller):
             message         = message,
             payload         = dict()
         )
+
+    def _get_csv(self, results):
+        csv = str()
+        if results:
+            csv += ','.join(key for key in results[0].keys())
+        csv = csv.rstrip(',')
+        csv += '\n'
+        for result in results:
+            for key, value in result.items():
+                csv += '{},'.format(value)
+            csv = csv.rstrip(',')
+            csv += '\n'
+        return csv
         
     @route('/ucis', type='json', auth='public', methods=['GET'])
     def get_ucis(self):
-        kwargs = response = request.jsonrequest
+        kwargs = request.jsonrequest
         receipt_number = kwargs.get('receipt_number') or False
         increament = kwargs.get('increment') or 1
         limit = kwargs.get('limit') or 1
@@ -21,7 +35,13 @@ class ScrappingUcis(Controller):
             return self._get_error('receipt number must be 13 digit')
 
         results = request.env['scrapping'].get_uscis_by_config(receipt_number, increament, limit)
+
+        csv = self._get_csv(results)
+        Response.headers = [
+            ('Content-Disposition', 'attachment; filename="report.csv"'),
+            ('Content-Type', 'text/csv;charset=utf8')
+        ]
         return dict(
             status_code     = 200,
-            payload         = results
+            payload         = csv
         )
