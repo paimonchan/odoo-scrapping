1f724c839dfa88c50ef6153671725b0895ef33f5
Paimon <n.h.shounen@gmail.com>
2023-07-02 12:31:29 +0700

    [NA] initial project

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Scrapping",
+    'summary': """Odoo Scrapping""",
+    'description': """Scrapping""",
+    'author': "Paimon",
+    'category': 'other',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+
+    ],
+}
